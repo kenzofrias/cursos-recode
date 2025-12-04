e8070c3d0898300c7d686ad0270c01f3d87d9341
Kenzo Friás <kenzofrias16@gmail.com>
2025-12-04 12:51:17 -0300

    .net e c#

---
+++ desenvolvimento-web/praticas-conteudos/myConsoleApp01/myConsoleApp01/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("As informações de agora são: " + DateTime.Now);
+Console.WriteLine("Meu nome é Kenzo Friás");
---
+++ desenvolvimento-web/praticas-conteudos/myConsoleApp02/myConsoleApp02/Program.cs
@@ -0,0 +1,40 @@
+﻿/*VARIAVEIS E COMANDOS ARITMETICOS E CONDICIONAIS*/
+
+// int var1 = 10;
+// int var2 = 20;
+// int var3 = 30;
+
+// Console.WriteLine(var1);
+// Console.WriteLine(var2);
+// Console.WriteLine(var3);
+
+// Console.WriteLine(var1 < var2);
+// Console.WriteLine(var2 == var3);
+
+// var1++;
+// var3--;
+
+// Console.WriteLine(var1);
+// Console.WriteLine(var3);
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dia = 10;
+        int mes = 20;
+        int ano = 30;
+
+        Console.WriteLine(dia);
+        Console.WriteLine(mes);
+        Console.WriteLine(ano);
+
+        Console.WriteLine(dia != mes);
+        Console.WriteLine(ano > mes);
+
+        Console.WriteLine(dia == ano);
+
+        int var = ano - mes;
+        Console.WriteLine( var == dia);
+    }
+}
